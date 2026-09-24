01c35440daa37a8c184d705be04435f19bc15dfd
raouanej <raouane.jaid@campusdavinci.edu.it>
2026-09-24 10:40:00 +0200

    formule

---
+++ formule di programma.py
@@ -0,0 +1,18 @@
+#creare una lista vuota
+listauno=[]
+#crea lista con elementi
+listadue=[1,2,3,4]
+#aggiungi un elemento alla lista(in coda)
+listauno.append(4)
+#visualizzare lunghezza della lista
+print(len(listauno))
+#visualizzare gli elementi di una lista
+print(listadue[0])
+print(listadue[1])
+print(listadue[2])
+print(listadue[3])
+#visualizzare gli elementi di una lista(for)
+
+
+for i in range(0,4):
+    print(listadue[i])
